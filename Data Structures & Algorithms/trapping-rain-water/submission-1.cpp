ec7efbcddd5966e6cc7fab7756c85792a9038797
Sultanali <duissebayev.sultanali@gmail.com>
2026-08-30 22:22:59 -0600

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+    int max_left = 0;
+    int max_right = 0;
+    int left = 0;
+    int right = height.size() - 1;
+    int water = 0;
+        while(left < right){
+            if(height[left] <= height[right]){
+                if(max_left < height[left]){
+                    max_left = height[left];
+                }
+                water += (max_left - height[left]);
+                left++;
+            }
+                
+            else if(height[left] >= height[right]){
+                if (max_right < height[right]){
+                    max_right = height[right];
+                }
+                water += (max_right - height[right]);
+                right--;
+            }
+           
+        }
+    return water;
+        
+
+        
+        
+    }
+};
